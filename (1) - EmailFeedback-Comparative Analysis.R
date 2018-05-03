e72c8cd78cf05308d7e47962e354bf9a32ab7290
E.C <35582713+bwcont@users.noreply.github.com>
2018-05-03 11:34:18 -0700

    Update (1) - EmailFeedback-Comparative Analysis.R

--- (1) - EmailFeedback-Comparative Analysis.R
+++ (1) - EmailFeedback-Comparative Analysis.R
@@ -8,5 +8,6 @@
 setwd("C:/")
 Q4Data <- read.csv("Q2Analysis.csv")
 View(Q4Data)
-
-
+attach(Q4Data)
+mod.1 <- glm(Email.Convo..Complete.1..Incomplete.0. ~ Email.Thread.Length + Mid.Final.Proportion.Similar + Email.Final.Proportion.Same.Codes + Total.Correct.Email.Codes + Total.Incorrect.Email.Codes)
+summary(mod.1)
